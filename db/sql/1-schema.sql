60589cbdfde1a1a9fe0e3718a7e8d395782993a4
Clement <clement.perrin9@etu.univ-lorraine.fr>
2023-02-28 16:23:16 +0100

    "schema" de la base de donnée (peut être bien j'en sais rien aled)

--- db/sql/1-schema.sql
+++ db/sql/1-schema.sql
@@ -7,28 +7,22 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `client`;
 CREATE TABLE `client` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idClient` int(11) NOT NULL AUTO_INCREMENT,
   `nom_client` varchar(128) NOT NULL,
   `mail_client` varchar(256) NOT NULL,
   `passwd` varchar(256) NOT NULL,
   `cumul_achats` decimal(8,2) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `commande`;
 CREATE TABLE `commande` (
-  `id` varchar(128) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
+  `idCmd` varchar(128) NOT NULL,
+  `dateCmd` date NOT NULL,
   `livraison` datetime NOT NULL,
-  `nom` varchar(128) NOT NULL,
-  `mail` varchar(256) NOT NULL,
   `montant` decimal(8,2) DEFAULT NULL,
   `remise` decimal(8,2) DEFAULT NULL,
-  `token` varchar(128) DEFAULT NULL,
   `client_id` int(11) DEFAULT NULL,
   `ref_paiement` varchar(128) DEFAULT NULL,
   `date_paiement` datetime DEFAULT NULL,
@@ -37,16 +31,30 @@ CREATE TABLE `commande` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `item`;
-CREATE TABLE `item` (
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE `produit` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uri` varchar(128) NOT NULL,
-  `libelle` varchar(128) DEFAULT NULL,
-  `tarif` decimal(8,2) DEFAULT NULL,
-  `quantite` int(11) DEFAULT 1,
-  `command_id` varchar(128) NOT NULL,
+  'categorie' varchar(128) NOT NULL,
+  `nom` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `prix` decimal(8,2) NOT NULL,
+  `image` varchar(256) DEFAULT NULL,
+  `stock` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `produit_commande`;
+CREATE TABLE `produit_commande` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produit_id` int(11) NOT NULL,
+  `commande_id` varchar(128) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
 
 
 -- 2019-11-08 13:47:44
