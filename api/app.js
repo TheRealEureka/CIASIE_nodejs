c3ae40997c25e4ab9584a2c0fd87612a5ef094ce
Eurêka <therealeureka@protonmail.com>
2023-03-03 12:09:51 +0100

    Aled

--- api/app.js
+++ api/app.js
@@ -16,7 +16,6 @@ Date.prototype.toDateInputValue = (function() {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/orders', orderRouter);
