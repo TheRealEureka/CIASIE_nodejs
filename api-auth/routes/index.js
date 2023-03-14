0c62012de8b64b076a5e9d28270613dd2b9390be
Eurêka <therealeureka@protonmail.com>
2023-03-14 17:53:52 +0100

    Il est 17:53

--- api-auth/routes/index.js
+++ api-auth/routes/index.js
@@ -15,55 +15,66 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', async (req, res, next) => {
-    const {email, password} = req.body;
-    let usr = await db('client').where({'mail_client': email});
-    if (usr.length > 0) {
-        bcrypt.compare(password, usr[0]['passwd'], async function (err, result) {
-            if (result) {
-                let data = {
-                    id: usr[0]['id'],
-                    email: usr[0]['mail_client'],
-                    name: usr[0]['nom_client'],
+    try {
+        const {email, password} = req.body;
+        let usr = await db('client').where({'mail_client': email});
+        if (usr.length > 0) {
+            bcrypt.compare(password, usr[0]['passwd'], async function (err, result) {
+                if (result) {
+                    let data = {
+                        id: usr[0]['id'],
+                        email: usr[0]['mail_client'],
+                        name: usr[0]['nom_client'],
+                    }
+                   // let cert = await fs.readFileSync('./private.key');
+                    let cert = "UrjklvnsjbejbVghvh";
+                    jwt.sign(data, cert, {expiresIn: '1h'}, function(err, token) {
+
+                        res.json({"access-token": token, "refresh-token": suid(16)});
+                    });
+                } else {
+                    res.status(401).json({
+                            "type": "error",
+                            "error": 401,
+                            "message": "no authorization header present"
+                        }
+                    );
                 }
-                let cert = await fs.readFileSync('private.key');
 
-                jwt.sign(data, cert, {expiresIn: '1h'}).then(token => {
-                    res.json({"access-token": token, "refresh-token": suid(16)});
-                });
-            } else {
-                res.status(401).json({
-                    "type": "error",
-                    "error": 401,
-                    "message": "no authorization header present"}
-                );
-            }
-
-        });
+            });
+        }
+    }catch (e) {
+        next(e)
     }
 });
 
 router.post('/signup', async (req, res, next) => {
    //new user
-    const {email, password, name} = req.body;
-    let usr = await db('client').where({'mail_client': email});
-    if (usr.length > 0) {
-        res.status(409).json({
-            "type": "error",
-            "error": 409,
-            "message": "user already exists"}
-        );
-    }
-    else {
-        const salt = await bcrypt.genSalt(saltRounds);
-        bcrypt.hash(password, salt, async function (err, hash) {
-            await db('client').insert({
-                'mail_client': email,
-                'passwd': hash,
-                'nom_client': name,
-                'created_at': Date.now(),
+    try {
+        const {email, password, name} = req.body;
+        let usr = await db('client').where({'mail_client': email});
+        if (usr.length > 0) {
+            res.status(409).json({
+                    "type": "error",
+                    "error": 409,
+                    "message": "user already exists"
+                }
+            );
+        } else {
+            const salt = await bcrypt.genSalt(saltRounds);
+            bcrypt.hash(password, salt, async function (err, hash) {
+                await db('client').insert({
+                    'mail_client': email,
+                    'passwd': hash,
+                    'nom_client': name,
+                    'created_at': new Date().toDateInputValue(),
+                });
+                res.json({message: 'user created'});
             });
-            res.json({message: 'user created'});
-        });
+        }
+    }catch (e)
+    {
+        next(e)
     }
 });
 
