b59a7c410aba1162fa13cf357c7638d7ff84729f
Eurêka <therealeureka@protonmail.com>
2023-03-21 15:15:01 +0100

    Il est 15:14

--- dbs/db_order/sql/1-schema.sql
+++ dbs/db_order/sql/1-schema.sql
@@ -5,18 +5,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE `client` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom_client` varchar(128) NOT NULL,
-  `mail_client` varchar(256) NOT NULL,
-  `passwd` varchar(256) NOT NULL,
-  `cumul_achats` decimal(8,2) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 DROP TABLE IF EXISTS `commande`;
 CREATE TABLE `commande` (
